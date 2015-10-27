b7c8861e6d01fa8ba50bed0a98437ce5cac56d3d
vitos <kvs1904@gmail.com>
2015-10-27 13:47:10 +0200

    Added module `categories`

---
+++ manana/modules/categories/__init__.py
@@ -0,0 +1,30 @@
+"""
+    :copyright: (c) 2015 by Vitaliy Kolesov, kvs1904+github@gmail.com.
+    :license: GPLv3, see LICENSE for more details.
+"""
+
+from . import views
+from . import utils
+from manana.application import ApplicationModule
+
+
+category = ApplicationModule(
+    'category',
+    __package__,
+)
+
+admin_category = ApplicationModule(
+    'admin_category',
+    __package__,
+    url_prefix='/admin',
+    static_folder='static',
+    template_folder='templates',
+)
+
+admin_category.add_url_rule('/categories/', view_func=views.categories)
+admin_category.add_url_rule('/ajax_tree/', view_func=views.ajax_tree)
+
+
+def register(app):
+    app.register_blueprint(admin_category)
+    app.before_first_request_funcs.append(utils.get_root_category)
---
+++ manana/modules/categories/views.py
@@ -0,0 +1,17 @@
+"""
+    :copyright: (c) 2015 by Vitaliy Kolesov, kvs1904+github@gmail.com.
+    :license: GPLv3, see LICENSE for more details.
+"""
+import json
+
+from manana.core.template import render_template
+from manana.modules.categories.utils import get_hierarchy_tree_of_categories
+
+
+def categories():
+    return render_template('admin_categories/index.html')
+
+
+def ajax_tree():
+    return json.dumps(get_hierarchy_tree_of_categories())
+
