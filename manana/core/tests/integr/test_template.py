5635c6f0fd558807916716aeb551cdd897bc1d5e
vitos <kvs1904@gmail.com>
2015-10-30 16:52:47 +0200

    was changed render_template tests

--- manana/core/tests/integr/test_template.py
+++ manana/core/tests/integr/test_template.py
@@ -6,20 +6,41 @@ from jinja2.environment import Template
 from manana.core.template import render_template
 
 
-def test_render_template(app):
-    tmp = 'text out of the block and {% block inner %}{{ vars }}{% endblock %}'
-    template = Template(tmp)
-
+def test_render_template_case1(app):
+    template = Template(
+        'text out of the block and {% block inner %}{{ vars }}{% endblock %}'
+    )
     case1 = render_template(
         template,
         vars='text inside the block'
     )
+    assert 'text out of the block and text inside the block' == case1
 
+
+def test_render_template_case2(app):
+    template = Template(
+        'text out of the block and {% block inner %}{{ vars }}{% endblock %}'
+    )
     case2 = render_template(
         template,
-        vars='text inside the block',
+        vars='TEST',
         block='inner'
     )
 
-    assert 'text out of the block and text inside the block' == case1
-    assert 'text inside the block' == case2
+    assert 'TEST' == case2
+
+
+def test_render_template_case3(app):
+    case3 = render_template(
+        Template(
+            """
+            {% block outer %}
+            {% block inner -%}
+                TEST
+            {%- endblock %}
+            {% endblock %}
+            """),
+        block='inner'
+    )
+
+    assert 'TEST' == case3
